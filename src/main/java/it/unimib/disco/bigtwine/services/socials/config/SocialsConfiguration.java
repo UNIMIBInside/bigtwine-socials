a3b74a7b9c982dbdc88668ffe8cfb4eb5c8d600e
Fausto <fausto91@gmail.com>
2019-04-09 08:01:35 +0200

    Implementato connectors per social sign in e sign up

---
+++ src/main/java/it/unimib/disco/bigtwine/services/socials/config/SocialsConfiguration.java
@@ -0,0 +1,65 @@
+package it.unimib.disco.bigtwine.services.socials.config;
+
+import com.mongodb.MongoClient;
+import com.netflix.discovery.EurekaClient;
+import it.unimib.disco.bigtwine.services.socials.connect.AccountCreatorLocator;
+import it.unimib.disco.bigtwine.services.socials.connect.AccountCreatorRegistry;
+import it.unimib.disco.bigtwine.services.socials.discovery.EurekaServiceAddressResolver;
+import it.unimib.disco.bigtwine.services.socials.discovery.ServiceAddressResolver;
+import it.unimib.disco.bigtwine.services.socials.discovery.SpringServiceAddressResolver;
+import org.eluder.spring.social.mongodb.MongoConnectionTransformers;
+import org.eluder.spring.social.mongodb.MongoUsersConnectionRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.security.crypto.encrypt.TextEncryptor;
+import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.UsersConnectionRepository;
+import org.springframework.social.connect.support.ConnectionFactoryRegistry;
+import org.springframework.social.twitter.connect.TwitterConnectionFactory;
+
+@Configuration
+public class SocialsConfiguration {
+
+    private final ApplicationProperties applicationProperties;
+
+    @Value("${spring.data.mongodb.database}")
+    private String databaseName;
+
+    public SocialsConfiguration(ApplicationProperties applicationProperties) {
+        this.applicationProperties = applicationProperties;
+    }
+
+    @Bean
+    public ConnectionFactoryLocator connectionFactoryLocator() {
+        ConnectionFactoryRegistry registry = new ConnectionFactoryRegistry();
+
+        registry.addConnectionFactory(new TwitterConnectionFactory(
+            this.applicationProperties.getSecurity().getTwitter().getConsumerKey(),
+            this.applicationProperties.getSecurity().getTwitter().getConsumerSecret()));
+
+        return registry;
+    }
+
+    @Bean
+    public UsersConnectionRepository getUserConnectionRepository(
+        MongoClient mongo,
+        ConnectionFactoryLocator connectionFactoryLocator,
+        TextEncryptor textEncryptor) {
+        MongoTemplate mongoTemplate = new MongoTemplate(mongo, databaseName);
+        MongoConnectionTransformers transformers = new MongoConnectionTransformers(connectionFactoryLocator, textEncryptor);
+        return new MongoUsersConnectionRepository(mongoTemplate, connectionFactoryLocator, transformers);
+    }
+
+    @Bean
+    public AccountCreatorLocator getAccountCreatorLocator() {
+        return new AccountCreatorRegistry();
+    }
+
+    @Bean
+    public ServiceAddressResolver getServiceAddressResolver(DiscoveryClient discoveryClient) {
+        return new SpringServiceAddressResolver(discoveryClient);
+    }
+}
---
+++ src/main/java/it/unimib/disco/bigtwine/services/socials/connect/AccountCreatorRegistry.java
@@ -0,0 +1,50 @@
+package it.unimib.disco.bigtwine.services.socials.connect;
+
+import it.unimib.disco.bigtwine.services.socials.connect.support.BasicFullNameSplitter;
+import org.springframework.social.twitter.api.Twitter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public final class AccountCreatorRegistry implements AccountCreatorLocator {
+
+    private final Map<Class<?>, AccountCreator> creatorsIndex = new HashMap<>();
+
+    public AccountCreatorRegistry() {
+        this.registerDefaultCreators();
+    }
+
+    private void registerDefaultCreators() {
+        this.addAccountCreator(
+            Twitter.class,
+            new TwitterAccountCreator(new BasicFullNameSplitter()));
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <A> AccountCreator<A> getAccountCreator(Class<A> apiClass) {
+        if (this.creatorsIndex.containsKey(apiClass)) {
+            return (AccountCreator<A>)this.creatorsIndex.get(apiClass);
+        }
+
+        return new DefaultAccountCreator<>();
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <A> AccountCreator<A> getAccountCreator(A api) {
+        return this.getAccountCreator((Class<A>)api.getClass());
+    }
+
+    @Override
+    public <A> void addAccountCreator(Class<A> apiClass, AccountCreator<A> creator) {
+        this.creatorsIndex.put(apiClass, creator);
+    }
+
+    @Override
+    public <A> void removeAccountCreator(Class<A> apiClass) {
+        this.creatorsIndex.remove(apiClass);
+    }
+
+}
