d25f0350484a0ba61e688a0e942e7d2d2a0a8307
Fausto <fausto91@gmail.com>
2019-11-07 22:02:27 +0100

    Utilizzo chiavi twitter dedicate BigTWINE

--- src/main/java/it/unimib/disco/bigtwine/services/socials/config/CryptoConfiguration.java
+++ src/main/java/it/unimib/disco/bigtwine/services/socials/config/CryptoConfiguration.java
@@ -1,15 +1,12 @@
 package it.unimib.disco.bigtwine.services.socials.config;
 
-import io.github.jhipster.config.JHipsterConstants;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.encrypt.Encryptors;
 import org.springframework.security.crypto.encrypt.TextEncryptor;
 
 @Configuration
-@Profile("!" + JHipsterConstants.SPRING_PROFILE_DEVELOPMENT)
 public class CryptoConfiguration {
     @Bean
     public TextEncryptor getTextEncryptor(
--- src/main/java/it/unimib/disco/bigtwine/services/socials/config/dbmigrations/DemoUsersMigration.java
+++ src/main/java/it/unimib/disco/bigtwine/services/socials/config/dbmigrations/DemoUsersMigration.java
@@ -5,22 +5,19 @@ import com.github.mongobee.changeset.ChangeSet;
 import org.eluder.spring.social.mongodb.MongoConnection;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
-import java.util.Date;
-
 @ChangeLog(order = "001")
 public class DemoUsersMigration {
     @ChangeSet(order = "01", author = "initiator", id = "01-addDemoUsers")
-    public void addAuthorities(MongoTemplate mongoTemplate) {
+    public void addDemoUsers(MongoTemplate mongoTemplate) {
         MongoConnection demoConnection = new MongoConnection();
         demoConnection.setUserId("demo");
         demoConnection.setProviderId("twitter");
         demoConnection.setProviderUserId("1190939321529966592");
         demoConnection.setDisplayName("@big_twine");
-        demoConnection.setAccessToken("1190939321529966592-f1wsxlZiXXKxiNYqMFaMadm4Vwu91S");
-        demoConnection.setSecret("f5rw1g3UKatRpKmByC3VqLDsStRGXAxwbVH2lH1E92zUi");
+        demoConnection.setAccessToken("470cd39ebe709016f30c3ba5438701b34a40159037d4a11a0565d57581a74d05800cd4fcfbe559555ff352648cb6e8ae19cbc2949d88c63aa27c5d5ace22ae6f1f63fe31670b2520102949691fb37a90");
+        demoConnection.setSecret("de0a6e0ca796b296d7644c4becb3ba2d53fefb6ef8afc2fcd373b05516335d6cb79002d0e26c0229ba6c8a23dce3bf0b69c51110719381005b10b9a66a3d4aa5");
         demoConnection.setProfileUrl("http://twitter.com/big_twine");
         demoConnection.setImageUrl("http://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png");
-        demoConnection.setCreated(new Date());
 
         mongoTemplate.save(demoConnection);
     }
